55bc2fcd5b3aceb48ca646dd4c27b6c471e92fb6
A1eksek <Alexey20080705@gmail.com>
2024-10-02 19:09:00 +0400

    Update pyt1.py

--- pyt1.py
+++ pyt1.py
@@ -10,3 +10,17 @@ local_time = local_time * 60
 
 time.sleep(local_time)
 print(text)
+
+
+import time
+from threading import Thread
+
+def SleepMe(i):
+    print('Поток %i засыпает на 5 секунд. \n' %i)
+    time.sleep(5)
+    print('Поток %i сейчас проснулся. \n' %i)
+'''Цикл вне функции!!!'''
+for i in range(10):
+    time.sleep(3)
+    th = Thread(target=SleepMe, args=(i,))
+    th.start()
